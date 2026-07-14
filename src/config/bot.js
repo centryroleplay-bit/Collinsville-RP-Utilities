5e351ff82e21b850b07bb4a713d560a6f7f19697
centryroleplay-bit <centryroleplay@gmail.com>
2026-07-13 22:13:39 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Watching Collinsville RP",     // this is what people actually see
+        state: "Serving Collinsville RP",     // this is what people actually see
         type: 3,               // Custom
       },
     ],
